235ff1bcbc4bd5cf8452dee9dad3928579ac17e8
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-06-02 15:37:38 +0300

    Algorithm completly rewritten

--- implementation/src/driver/Runner.java
+++ implementation/src/driver/Runner.java
@@ -5,23 +5,23 @@
 package driver;
 
 import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
 import java.util.ArrayList;
-
-import java.util.List;
-
-
 import excel.io.ReadExcel;
 import jxl.read.biff.BiffException;
+import utilities.Coordinate;
 import utilities.Map;
 
 
 public class Runner {
-
     public static void main(String[] args) {
     	ReadExcel reader = new ReadExcel();
     	reader.setInputFile("atm.xls");
     	
+    	final int MACHINES_COUNT = 6; // <= data.length()
+    	Map map = new Map(MACHINES_COUNT);
+    	map.setCoordinate(Coordinate.GEOGRAFIC_CENTER);
+    	
+
 
     	try {
     		
@@ -29,12 +29,12 @@ public class Runner {
     		final int DATA_TYPES_COUNT = 3;
     		final int RECORDS_COUNT = reader.getSheetRows();
 
-    		final int HOUR_SIZE = 60;
     		Object [][] data = new Object [RECORDS_COUNT][DATA_TYPES_COUNT];//String and integers array
     		
     		reader.extractEssentials(data);
     		
-    		for(int i = 1 ; i < data.length ; i++) {
+    		int coordinateIndex = 1; //Skip the geographic center
+    		for(int i = 1 ; coordinateIndex < MACHINES_COUNT ; i++) {//i = 1 skip the headers
     			Object [] curAtmData = data[i];
     			
     			//Ignore invalid data
@@ -43,71 +43,25 @@ public class Runner {
     			String city = (String)curAtmData[0];
     			double x = Double.parseDouble((String)curAtmData[1]);
     			double y = Double.parseDouble((String)curAtmData[2]);
+    			
+    			map.setCoordinate(coordinateIndex++, x,y , city);
+
+
+  
+    			
     		}
     		
     		// ====================Algorithm============================
-    		
-    		
-//    		ATM a1 =  new ATM("באר שבע " , 35 ,34);
-//    		ATM a2 =  new ATM("באר שבע " , 35 ,34);
-//    		ATM a3 =  new ATM("באר שבע " , 35 ,34);
-//    		ATM a4 =  new ATM("באר שבע " , 35 ,34);
-//    		ATM center = ATM.GEOGRAFIC_CENTER;
-//    		
-//    		Graph g = new Graph();
-//    		g.addATM(a1);
-//    		g.addATM(a2);
-//    		g.addATM(a3);
-//    		g.addATM(a4);
-//    		g.addATM(center);
-//    		
-//    		new Route (a1 , a2);
-//    		new Route (a1 , a3);
-//    		new Route (a1 , a4);
-//    		new Route (a1 , center);
-//    		new Route (a2 , a3);
-//    		new Route (a2 , a4);
-//    		new Route (a2 , center);
-//    		new Route (a3 , a4);
-//    		new Route (a3 , center);
-//    		new Route (a4 , center);
-//    		System.out.println(g);
-//    		
-//    		Graph g2 = new Graph();
-//    		g2.graphName = "second";
-//    		g2.addATM(a1.createIsolatedATM());
-//    		g2.addATM(a2.createIsolatedATM());
-//    		g2.addATM(a3.createIsolatedATM());
-//    		g2.addATM(a4.createIsolatedATM());
-//    		g2.addATM(center.createIsolatedATM());
-//    		
-//    		g2.addRoute(1, 2);
-//    		g2.addRoute(1, 3);
-//    		g2.addRoute(3, 4);
-//    		g2.addRoute(4, 5);
-//
-//    		System.out.println(g2);
-//
-//    		System.out.println(g2.isDegenerated());
-    		
-    		
-    		
-    		Map m = new Map(5);
-    		try {
-				m.addPath(4, 1);
-				m.addPath(3, 2);
-				m.addPath(4, 2);
-				m.addPath(4, 3);
 
-				
-			} catch (InvalidAlgorithmParameterException e) {
-				e.printStackTrace();
-			}
-    		
-    		Map bestJourney = findBestJourney(m);
+    		final int HOUR_SIZE = 60;
+
+    		Map bestJourney = findBestJourney(map);
     		
     		System.out.println("lightest journey is:\n" + bestJourney  );
-    		System.out.println("Minimal weight: " + bestJourney.weight());
+    		double time = bestJourney.totalTime() / HOUR_SIZE;
+    		
+            System.out.println("Minimum time: "+String.format("%.2f",time) + " hours");
+
     		
 
 
@@ -121,52 +75,20 @@ public class Runner {
     
     
 
-    public static Map findBestJourney(Map g) {
-    	ArrayList<String> permutations = (ArrayList<String>) g.findPermutations();
+    //====================== Algorithm high view ============================
+    public static Map findBestJourney(Map m) {
+    	ArrayList<String> permutations = (ArrayList<String>) m.findPermutations();
     	double minWeight = Double.MAX_VALUE;
     	Map minGraph = null;
     	for(String currentPermutation : permutations) {
-    		Map currentGraph = g.stringToGraph(currentPermutation);
+    		Map currentMap = m.stringToMap(currentPermutation);
     		
-    		if(currentGraph.weight() < minWeight && currentGraph.isDegenerated()) {
-    			minGraph = currentGraph;
-    			minWeight = currentGraph.weight();
+    		if(currentMap.totalWeight() < minWeight && currentMap.isDegenerated()) {
+    			minGraph = currentMap;
+    			minWeight = currentMap.totalWeight();
     		}
     	}
     	return minGraph;
-    	
     }
-    
-
-    
-    
-
-    
-    public static void printLongString  (String str) {
-    	
-		int strLen = str.length();
-		final int ROW_SIZE = 100;
-		int base = -ROW_SIZE , limit = 0;
-		
-		while(limit < strLen) {
-			base += ROW_SIZE;
-
-			if(limit + ROW_SIZE  > strLen)
-				limit += strLen % ROW_SIZE;
-			else
-				limit += ROW_SIZE;
-
-			System.out.println(str.substring(base , limit - 1));
-			
-			
-			
-			System.out.println();
-		}
-    	
-    	
-    }
-
-    
-
-
+    //=====================================================================
 }
--- implementation/src/utilities/Path.java
+++ implementation/src/utilities/Path.java
@@ -1,38 +1,43 @@
+/*Authors : Yulia Moshan 319565610
+ * 			Gil Pasi 	 206500936
+ * Algorithm planning - assignment 3 
+ * */
 package utilities;
 
 public class Path {
 	//Equivalent to an edge on a graph
 	
 	
-	private int v , u ;
+	private Coordinate v , u ;
 	boolean isVisited = false;
-	private double weight;
+	private double time = 1; //Default value
+
 	
 	
 	//----Constructors----
-	public Path (int v , int u ){
+	public Path (Coordinate v , Coordinate u ){
 		this.v = v ;
 		this.u = u;
-		this.weight = 1;
+		this.time = v.time(u);
 	}
 	
-	public Path (int u , int v , double weight){
-		this.v = v ;
-		this.u = u;
-		this.weight = weight;
-	}
 	
 	public Path (Path other) {
 		this.v = other.v;
 		this.u = other.u;
-		this.weight = other.weight;
+		this.time = other.time;
 
 	}
 
 	//----Accessors----
-	public int getV() {return v;}
-	public int getU() {return u;}
-	public double getWeight () {return weight;}
+	public Coordinate getV() {return v;}
+	public Coordinate getU() {return u;}
+	public double getVX() {return v.getX();}
+	public double getVY() {return v.getY();}
+	public double getUX() {return u.getX();}
+	public double getUY() {return u.getY();}
+	
+	public double getTime () {return time;}
 	
 	public boolean equals(Path other) {
 		return(
